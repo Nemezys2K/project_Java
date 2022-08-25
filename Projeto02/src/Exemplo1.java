b69f67e1b1d3788654fa1e45e5efd22666d709d0
Nemezys2k <carvalho2604@outlook.com>
2022-08-25 20:17:15 -0300

    project2

---
+++ Projeto02/src/Exemplo1.java
@@ -0,0 +1,14 @@
+//Exemplo 1
+//A classe representa um objeto.
+//A classe deve começar com letra maiúscula
+//O JAVA é case sensitive, diferencia maiúsculas de minúsculas
+public class Exemplo1 {
+	//Metodo principal da class
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Exibição com quebra de linha...
+		System.out.println("Olá usuário..!");
+		System.out.println("Bem vindo ao universo JAVA!");
+	}
+
+}
